5112f45259b4ab04aa6051c49140ad1224e1b9b2
Meyre França <meyreanusia@gmail.com>
2025-08-24 20:38:41 -0300

    Menu navegação

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from './page/login'
 import './App.css'
 import Register from "./page/register";
+import MenuNavbar from "./page/menuNavegacao";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/navegacao" element={<MenuNavbar/>}></Route>
    </Routes>
   )
 }
